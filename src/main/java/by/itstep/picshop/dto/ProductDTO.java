1838c6990bb26bf4decf9d932a63f95373e4fe98
Ivan S <marioinmilo@mail.ru>
2022-11-14 21:09:10 +0300

    add List category

--- src/main/java/by/itstep/picshop/dto/ProductDTO.java
+++ src/main/java/by/itstep/picshop/dto/ProductDTO.java
@@ -1,16 +1,21 @@
 package by.itstep.picshop.dto;
 
+import by.itstep.picshop.model.Category;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProductDTO {
     private Long id;
     private String title;
     private BigDecimal price;
+    private List<Category> categories;
 }
