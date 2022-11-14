b1245ecb4a98cdeb0713afed7fbcb117e8ba23d9
Ivan S <marioinmilo@mail.ru>
2022-11-14 20:59:54 +0300

    Add findFirstByEmail, findFirstByRole

--- src/main/java/by/itstep/picshop/repository/UserRepository.java
+++ src/main/java/by/itstep/picshop/repository/UserRepository.java
@@ -2,7 +2,18 @@ package by.itstep.picshop.repository;
 
 import by.itstep.picshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findFirstByName(String name);
+
+    User findFirstByEmail(String email);
+
+    User findFirstByRole(String role);
+
+//    @Query(name = "UPDATE users SET role := 'roles' WHERE id := var")
+//    User roleUpdateUser(@Param("role") String role, @Param("var") Integer id);
+//
+//    User updateRoleById(String role, Long id);
 }
