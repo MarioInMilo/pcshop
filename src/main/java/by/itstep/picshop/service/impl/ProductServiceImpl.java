72d9f286a0c57bf394b762b3b9029ed856f388a2
Ivan S <marioinmilo@mail.ru>
2022-11-14 21:10:16 +0300

    update getAll

--- src/main/java/by/itstep/picshop/service/impl/ProductServiceImpl.java
+++ src/main/java/by/itstep/picshop/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package by.itstep.picshop.service.impl;
 
 import by.itstep.picshop.dto.ProductDTO;
+import by.itstep.picshop.mapper.ProductMapper;
 import by.itstep.picshop.repository.ProductRepository;
 import by.itstep.picshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +11,21 @@ import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductMapper mapper = ProductMapper.MAPPER;
+
+    @Autowired
+    private ProductRepository repository;
+
+
     @Override
     public List<ProductDTO> getAll() {
-        return null;
+        return mapper.fromProductList(repository.findAll());
     }
 
-//    @Override
-//    public List<ProductDTO> getAll() {
-//        return productMapper.fromProductList(repository.findAll());
-//    }
+    @Override
+    public void addToUserBasket(Long productId, String username) {
+
+    }
+
 }
