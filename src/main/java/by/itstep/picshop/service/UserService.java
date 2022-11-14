782d5cc655c90cf86078b34d4b46df139ad06a68
Ivan S <marioinmilo@mail.ru>
2022-11-14 20:59:16 +0300

    Add findByName, updateProfile, deleteUser, existUserById, createUser, updateRole

--- src/main/java/by/itstep/picshop/service/UserService.java
+++ src/main/java/by/itstep/picshop/service/UserService.java
@@ -1,5 +1,7 @@
 package by.itstep.picshop.service;
 
+import by.itstep.picshop.bean.request.CreateUserRequest;
+import by.itstep.picshop.bean.response.CreateUserResponse;
 import by.itstep.picshop.dto.UserDTO;
 import by.itstep.picshop.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,5 +14,14 @@ public interface UserService extends UserDetailsService { //Security
     List<UserDTO> getAll();
     Optional<UserDTO> getById(Long id);
     List<UserDTO> updateUser(User user);
+    User findByName(String name);
+    void updateProfile(UserDTO userDTO);
+    void deleteUser(UserDTO userDTO);
+    boolean existUserById(Long id);
 
+
+
+    CreateUserResponse createUser(CreateUserRequest request);
+
+    UserDTO updateRole(long id, String role);
 }
