55764d8462173315b807fc7f4ae035fd71f986e7
Ivan S <marioinmilo@mail.ru>
2022-12-10 21:20:17 +0300

    Refinement 'UseMapper.java'

--- src/main/java/by/itstep/picshop/mapper/UseMapper.java
+++ src/main/java/by/itstep/picshop/mapper/UseMapper.java
@@ -6,6 +6,7 @@ import by.itstep.picshop.model.User;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -14,19 +15,17 @@ import java.util.List;
 @Mapper(componentModel = "spring", imports = {BCryptPasswordEncoder.class, SecurityConfig.class})
 public interface UseMapper {
     UseMapper MAPPER = Mappers.getMapper(UseMapper.class);
+
     @Mapping(target = "password", expression = "java(SecurityConfig.passwordEncoder().encode(userDTO.getPassword()))")
     @Mapping(target = "name", source = "username")
-//    @Mapping(target = "password", expression = "java(new BCryptPasswordEncoder().encode(userDTO.getPassword()))")
     User toUser(UserDTO userDTO);
 
     @InheritInverseConfiguration
     @Mapping(target = "matching", ignore = true)
-//    @Mapping(target = "password", expression = "java(new BCryptPasswordEncoder().encode(user.getPassword()))")
-    // securityConfig.passwordEncoder().encode(userDTO.getPassword())
+    @Mapping(target = "newPassword", ignore = true)
     @Mapping(target = "username", source = "name")
     @Mapping(target = "password", expression = "java(SecurityConfig.passwordEncoder().encode(user.getPassword()))")
     UserDTO fromUser(User user);
-
     List<User> toUserList(List<UserDTO> userDTOS);
 
     List<UserDTO> fromUserList(List<User> users);
