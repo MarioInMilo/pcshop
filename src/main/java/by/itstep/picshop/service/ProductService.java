1b258ca016c0e56bde9844daf57ca7b6b3abb674
Ivan S <marioinmilo@mail.ru>
2022-12-10 21:23:43 +0300

    Adding 'saveProduct', 'getById', 'save', 'createProduct', 'editProduct', 'deleteProduct', 'existProductById', 'weaningAfterPurchase'

--- src/main/java/by/itstep/picshop/service/ProductService.java
+++ src/main/java/by/itstep/picshop/service/ProductService.java
@@ -1,10 +1,35 @@
 package by.itstep.picshop.service;
 
+import by.itstep.picshop.dto.BasketDTO;
 import by.itstep.picshop.dto.ProductDTO;
+import by.itstep.picshop.model.Basket;
+import by.itstep.picshop.model.Product;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductService {
     List<ProductDTO> getAll();
+
     void addToUserBasket(Long productId, String username);
+
+    void saveProduct(Product product, MultipartFile fileOne, MultipartFile fileTwo, MultipartFile fileThree) throws IOException;
+
+    Optional<ProductDTO> getById(Long id);
+
+    void save(ProductDTO productDTO);
+
+    boolean createProduct(ProductDTO productDTO);
+
+    void editProduct(ProductDTO productDTO);
+
+    void deleteProduct(ProductDTO productDTO);
+
+    boolean existProductById(Long id);
+
+    void weaningAfterPurchase(BasketDTO basket);
+
+
 }
