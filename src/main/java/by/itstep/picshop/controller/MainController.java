8c485dcebdcda05ef8a6ece1a6c0b5d780e23d57
Ivan S <marioinmilo@mail.ru>
2022-12-10 21:36:31 +0300

    Adding 'addbasket'. Refinement of 'index'

--- src/main/java/by/itstep/picshop/controller/MainController.java
+++ src/main/java/by/itstep/picshop/controller/MainController.java
@@ -1,14 +1,36 @@
 package by.itstep.picshop.controller;
 
+import by.itstep.picshop.dto.ProductDTO;
+import by.itstep.picshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+import java.util.List;
+
 @Controller
+@RequestMapping({"", "/", "/main"})
 public class MainController {
-    @RequestMapping({"", "/"})
-    public String index() {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public String index(Model model) {
+        List<ProductDTO> productDTOS = productService.getAll();
+        model.addAttribute("products", productDTOS.size() == 0 ? new ProductDTO() : productDTOS.get(0));
         return "index";
+    }
+
+    @GetMapping("/{id}/basket")
+    public String addBasket(@PathVariable Long id, Principal principal) {
+        if (principal == null) return "redirect:/login";
+        productService.addToUserBasket(id, principal.getName());
+        return "redirect:/";
     }
 
     @RequestMapping("/login")
@@ -17,7 +39,7 @@ public class MainController {
     }
 
     @RequestMapping("login-error")
-    public String LoginError(Model model){
+    public String LoginError(Model model) {
         model.addAttribute("loginError", true);
         return "login";
     }
