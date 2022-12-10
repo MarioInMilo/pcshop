a8511c3640439fa00eabadb4abe0618667c9e412
Ivan S <marioinmilo@mail.ru>
2022-12-10 21:38:28 +0300

    Adding 'quantity', 'productInfo', 'images', 'previewImageId'

--- src/main/java/by/itstep/picshop/model/Product.java
+++ src/main/java/by/itstep/picshop/model/Product.java
@@ -1,15 +1,13 @@
 package by.itstep.picshop.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
-
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -27,9 +25,30 @@ public class Product {
     private String title;
 
     private BigDecimal price;
+
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "products_categories",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @ToString.Exclude
     private List<Category> categories;
+
+    private Integer quantity;
+
+
+    private String productInfo;
+
+    @OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "product")
+    @ToString.Exclude
+    private List<ProductImage> images;
+
+    private Long previewImageId;
+
+    public void addImageToProduct(ProductImage image) {
+        image.setProduct(this);
+        images.add(image);
+    }
+
 }
