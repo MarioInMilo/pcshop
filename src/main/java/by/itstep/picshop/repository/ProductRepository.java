3cd7f36a0fccd4d1ded099ab6469928333d08dda
Ivan S <marioinmilo@mail.ru>
2022-12-10 21:24:35 +0300

    Adding 'deleteNullQuantityProduct', 'findByTitle'

--- src/main/java/by/itstep/picshop/repository/ProductRepository.java
+++ src/main/java/by/itstep/picshop/repository/ProductRepository.java
@@ -2,6 +2,15 @@ package by.itstep.picshop.repository;
 
 import by.itstep.picshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Modifying
+    @Query(nativeQuery = true, value = "DELETE FROM products p WHERE p.quantity <= 0")
+    void deleteNullQuantityProduct();
+
+    Product findByTitle(String title);
 }
